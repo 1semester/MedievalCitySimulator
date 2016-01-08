6abd6176928e8a40a60c75c5330a430ad8fa8dff
NielsJustesen <niels449@hotmail.com>
2016-01-08 10:00:55 +0100

    Building kodekommentarer

--- medCitySim/medCitySim/Buildings/Building.cs
+++ medCitySim/medCitySim/Buildings/Building.cs
@@ -13,16 +13,23 @@ namespace MedCitySim
         protected List<Citizen> citizens;
         protected ISoundEngine engine;
         private float workInterval;
-
-        public Building(string imagepPath, Vector2D position): base (imagepPath,position)
-        {
-        }
-        protected virtual void Work()
-        {
-        }
-        protected virtual void Cost()
-        {
-        }
+        /// <summary>
+        /// This construter makes sure that all buildings have an imagePath and a position.
+        /// </summary>
+        /// <param name="imagepPath"></param>
+        /// <param name="position"></param>
+        public Building(string imagepPath, Vector2D position): base (imagepPath,position){}
+        /// <summary>
+        /// All buildings have a unique work method.
+        /// </summary>
+        protected virtual void Work(){}
+        /// <summary>
+        /// All buildings have a unique Cost method.
+        /// </summary>
+        protected virtual void Cost(){}
+        /// <summary>
+        /// This method holds the sound that is played when the player builds a building.
+        /// </summary>
         protected void BuildSound()
         {
             try
@@ -32,6 +39,11 @@ namespace MedCitySim
             }
             catch(Exception ex) { }
         }
+        /// <summary>
+        /// The worktimer controles the time it takes to make a ressource.
+        /// </summary>
+        /// <param name="WorkInterval"></param>
+        /// <returns></returns>
         protected virtual float WorkTimer(float WorkInterval)
         {
             float WorkTimer = WorkInterval;
