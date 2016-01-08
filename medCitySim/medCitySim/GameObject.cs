537015623c333168ad2521ce630d736d7406c30e
NielsJustesen <niels449@hotmail.com>
2016-01-08 09:48:06 +0100

    GameObject kodekommentarer

--- medCitySim/medCitySim/GameObject.cs
+++ medCitySim/medCitySim/GameObject.cs
@@ -33,7 +33,8 @@ namespace MedCitySim
                 position = value;
             }
         }
-
+        /// <param name="imagePath"> Used to write the path for the image </param>
+        /// <param name="startPosition"> The Vector2D class takes an X and a Y coordinate for where the sprites is drawn from </param>
         public GameObject(string imagePath, Vector2D startPosition)
         {
             string[] imagePaths = imagePath.Split(';');
@@ -46,15 +47,28 @@ namespace MedCitySim
             this.sprite = this.animationFrames[0];
             this.position = startPosition;
         }
+        /// <summary>
+        /// The update checks for collision for each GameObject the inherrits this method.
+        /// </summary>
+        /// <param name="currentFPS"></param>
         public virtual void Update(float currentFPS)
         {
             CheckCollosion();
         }
+        /// <summary>
+        /// Draw uses the Graphics to draw the sprites
+        /// </summary>
+        /// <param name="dc"> the graphics </param>
         public virtual void Draw(Graphics dc)
         {
             dc.DrawImage(sprite, position.X, position.Y, sprite.Width, sprite.Height);
             dc.DrawRectangle(new Pen(Brushes.Red), CollisionBox.X, CollisionBox.Y, CollisionBox.Width, CollisionBox.Height);
         }
+        /// <summary>
+        /// Detects wether or not two GameObjects collide
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public bool IsCollidingWith(GameObject other)
         {
             if ((CollisionBox.IntersectsWith(other.CollisionBox)))
@@ -66,6 +80,9 @@ namespace MedCitySim
                 return false;
             }
         }
+        /// <summary>
+        /// Checks the collsion on GameObjects
+        /// </summary>
         private void CheckCollosion()
         {
             foreach (GameObject go in GameWorld.Objs)
@@ -79,6 +96,11 @@ namespace MedCitySim
                 }
             }
         }
+        /// <summary>
+        /// This method is virtual, so that all GameObjects can inherrit it, but dont have too.
+        /// When inherrited, the individual GameObject has a unique string of code for the event.
+        /// </summary>
+        /// <param name="other"></param>
         protected virtual void OnCollision(GameObject other)
         {
         }
