3cda3848cec4d32e4a552acab20117ce115bfd88
NielsJustesen <niels449@hotmail.com>
2016-01-08 09:53:53 +0100

    UserInterface kode kommentarer

--- medCitySim/medCitySim/UserInterface.cs
+++ medCitySim/medCitySim/UserInterface.cs
@@ -8,6 +8,11 @@ namespace MedCitySim
 {
     class UserInterface : GameObject
     {
+        /// <summary>
+        /// solely a visual class, made to be drawn for player appreciation.
+        /// </summary>
+        /// <param name="imagePath"></param>
+        /// <param name="startPosition"></param>
         public UserInterface(string imagePath, Vector2D startPosition) : base (imagePath, startPosition)
         {
 
